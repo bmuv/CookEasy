2441278df31c748cb0e500c23a252b6ee2408a03
Brice M. Muvunyi <movebrice@gmail.com>
2024-06-02 22:28:07 -0400

    Improved UI

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from app import app, db
 from app.forms import InventoryForm, RegistrationForm, LoginForm
 from app.models import User, Inventory
