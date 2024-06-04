28225460b5c7f1def44bea435c7cae180f518d87
Brice Muvunyi <movebrice@gmail.com>
2024-06-04 19:42:09 -0400

    renamed .env file from .env_var

--- app/utils/spoonacular.py
+++ app/utils/spoonacular.py
@@ -4,7 +4,6 @@ import requests
 
 API_KEY = os.environ.get('SPOONACULAR_API_KEY')
 
-
 def fetch_recipes(ingredients):
     url = f'https://api.spoonacular.com/recipes/findByIngredients?ingredients={",".join(ingredients)}&apiKey={API_KEY}'
     response = requests.get(url)
--- load_env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def load_env():
-    with open('.env_var') as f:
-        for line in f:
-            if line.strip() and not line.startswith('#'):
-                key, value = line.strip().split('=', 1)
-                os.environ[key] = value
