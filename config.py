e980ef3db616ab5cd480765702c602c6fdba7e18
Brice M. Muvunyi <movebrice@gmail.com>
2024-06-06 15:10:12 -0400

    Modified DB URI to point to POSTGRES

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SPOONACULAR_API_KEY = os.environ.get('SPOONACULAR_API_KEY')
